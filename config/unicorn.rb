7e4d2711be4bc9eb734013c001ab055e6930327d
osawa-26 <oswtmj@gmail.com>
2020-07-20 21:03:55 +0900

    0720_12

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,7 @@ app_path = File.expand_path('../../../', __FILE__)
 worker_processes 1
 working_directory "#{app_path}/current"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "/var/www/study_meter/shared/tmp/pids/unicorn.pid"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 timeout 60
