4f21a45b895122023717364ee414b2e4b7ccc57c
osawa-26 <oswtmj@gmail.com>
2020-07-21 18:31:48 +0900

    0721_13

--- app/forms/admin/app_user_form.rb
+++ app/forms/admin/app_user_form.rb
@@ -1,34 +1,34 @@
-# class Admin::AppUserForm
-#   include ActiveModel::Model
-#
-#   attr_accessor :app_user
-#   delegate :persisted?, :save, to: :app_user
-#
-#   def initialize(app_user = AppUser.find(params[:id]))
-#     @app_user = app_user
-#   end
-#
-#   def assign_attributes(params = {})
-#     @params = params
-#     app_user.assign_attributes(app_user_params)
-#   end
-#
-#   private def app_user_params
-#     @params.require(:app_user).permit(
-#       :email,
-#       :family_name,
-#       :given_name,
-#       :family_name_kana,
-#       :given_name_kana,
-#       :nickname,
-#       :gender,
-#       :birthday,
-#       :prefecture,
-#       :target,
-#       :password,
-#       :start_date,
-#       :end_date,
-#       :suspended
-#     )
-#   end
-# end
+class Admin::AppUserForm
+  include ActiveModel::Model
+
+  attr_accessor :app_user
+  delegate :persisted?, :save, to: :app_user
+
+  def initialize(app_user = AppUser.find(params[:id]))
+    @app_user = app_user
+  end
+
+  def assign_attributes(params = {})
+    @params = params
+    app_user.assign_attributes(app_user_params)
+  end
+
+  private def app_user_params
+    @params.require(:app_user).permit(
+      :email,
+      :family_name,
+      :given_name,
+      :family_name_kana,
+      :given_name_kana,
+      :nickname,
+      :gender,
+      :birthday,
+      :prefecture,
+      :target,
+      :password,
+      :start_date,
+      :end_date,
+      :suspended
+    )
+  end
+end
