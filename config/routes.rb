f1a5ff38240051fb53cad5eaa9d0f671246a89de
osawa-26 <oswtmj@gmail.com>
2020-07-23 14:33:52 +0900

    0723_6

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,21 @@
 Rails.application.routes.draw do
-  # config = Rails.application.config.study_meter
 
-  # constraints host: "study-meter.com" do
-    namespace :user, path: "" do
-      root "toppages#index"
-      get "login" => "sessions#new", as: :login
-      # delete "records" => "records#destroy"
+  namespace :user, path: "" do
+    root "toppages#index"
+    get "login" => "sessions#new", as: :login
+    resource :session, only: [ :create, :destroy ]
+    resource :account, except: [ :new, :create, :destroy ]
+    resource :password, only: [ :show, :edit, :update ]
+    resources :records
+  end
 
-      resource :session, only: [ :create, :destroy ]
-      resource :account, except: [ :new, :create, :destroy ]
-      resource :password, only: [ :show, :edit, :update ]
+  namespace :admin do
+    root "top#index"
+    get "login" => "sessions#new", as: :login
+    resource :session, only: [ :create, :destroy ]
+    resources :app_users do
       resources :records
     end
-
-    namespace :admin do
-      root "top#index"
-      get "login" => "sessions#new", as: :login
-      resource :session, only: [ :create, :destroy ]
-      resources :app_users do
-        resources :records
-      end
-      resources :records
-    end
-  # end
+    resources :records
+  end
 end
